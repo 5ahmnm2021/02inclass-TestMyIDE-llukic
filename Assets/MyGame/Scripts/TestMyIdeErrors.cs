04f0a637a3a7d523296f1a71101086449484567b
lealukic <lea.lukic@htl-salzburg.ac.at>
2021-02-23 10:10:08 +0100

    firstcommit

---
+++ Assets/MyGame/Scripts/TestMyIdeErrors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestMyIdeErrors : MonoBehaviour
+{
+    string Tag = "TestMyIdeErrors";
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    float d = 5;
+
+    TextMeshProUGUI TextPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int c = a + b;
+        Debug.Log(message:"a + b = c");
+        Debug.Log(Tag + "- Done ;)");
+    }
+
+  
+}
